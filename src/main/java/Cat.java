0b693b5d3879768b146cd63b66c46f413fac3d5b
Evgeny Dubrovin <lukinova.anastasiya@mail.ru>
2024-10-18 17:58:06 +0300

    done

--- src/main/java/Cat.java
+++ src/main/java/Cat.java
@@ -13,23 +13,23 @@ public class Cat {
         this.voice = voice;
     }
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Cat cat = (Cat) o;
-        return Objects.equals(voice, cat.voice);
-    }
-
-    @Override
-    public String toString() {
-        return "Cat{" +
-                "voice='" + voice + '\'' +
-                '}';
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(voice);
-    }
+//    @Override
+//    public boolean equals(Object o) {
+//        if (this == o) return true;
+//        if (o == null || getClass() != o.getClass()) return false;
+//        Cat cat = (Cat) o;
+//        return Objects.equals(voice, cat.voice);
+//    }
+//
+//    @Override
+//    public String toString() {
+//        return "Cat{" +
+//                "voice='" + voice + '\'' +
+//                '}';
+//    }
+//
+//    @Override
+//    public int hashCode() {
+//        return Objects.hash(voice);
+//    }
 }
